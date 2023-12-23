5c889fedfa860bd51a18512009c02d4bf97ccc99
Stephen Benner <developer@sensiblesolutions.me>
2023-12-22 19:27:19 -0800

    advent of code - day 4

--- advent-of-code/4-2-scratchcards.rb
+++ advent-of-code/4-2-scratchcards.rb
@@ -1,18 +1,16 @@
-# 4-1: Scratchcards
+# 4-2: Scratchcards
 MATRIX = IO.readlines '/Users/sven/github/kata/advent-of-code/4-1-input'
 NUM = /(\d{1,2})/
+$cards = 0
 p = MATRIX.reduce(0) do |sum, card|
+  card_num = card.match(/Card (\d{1,3})/)[0]
   winning_numbers = card.match(/Card *\d{1,3}:(.*)\|/)[0].scan(NUM).flatten
   winning_numbers.shift until winning_numbers.count === 10
   numbers = card.match(/\|  ?(\d+(?:  ?\d+)*)/)[0].scan(NUM).flatten
-  wn = numbers.reduce([]) do |nums, n|
-    nums << n if winning_numbers.include? n
-    nums
-  end
-  if wn.count === 0
-    sum
-  else
-    sum + 2**(wn.count-1)
-  end
+  wn = numbers.select {| n| winning_numbers.include? n }.count
+  card_copies = (card_num+1..card_num+wn).to_a
+  card_copies.each { |c| sum += 1 }
+  
+  sum
 end
 puts p
